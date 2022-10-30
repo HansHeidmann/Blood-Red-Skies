3e4a7d57e0dec42593aed0c08b7055940e3dc6d8
Hans Heidmann <hansheidmann@gmail.com>
2022-10-29 21:49:25 -0700

    concept art and architecture diagram

---
+++ public/modules/char.js
@@ -0,0 +1,14 @@
+class char {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        sprite = new Image;
+        sprite.src = "img/char.png";
+
+    }
+
+    
+}
---
+++ public/modules/main.js
@@ -0,0 +1,20 @@
+import * as classes from "/modules/"
+
+//
+// Top Down Survival Shooter
+// 
+// Hans Heidmann & Peter Loden
+//
+
+
+
+function run() {
+
+    // load the game
+    scene = new scene(800, 800);//       -      -     -    -   -   -  - -   //---//>     
+    scene.load();
+    ////
+    ////
+    ////
+
+}
